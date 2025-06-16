16de862b4a2d096e92684e31d7c7d30a913bf77c
TonnyAxel <80761341+TonnyAxel23@users.noreply.github.com>
2025-06-16 17:16:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from twilio.twiml.messaging_response import MessagingResponse
 import sqlite3
+import os
 from datetime import datetime, date
 
 app = Flask(__name__)
@@ -217,5 +218,5 @@ def dashboard():
 # ===========================
 # ▶️ Run the App
 # ===========================
-if __name__ == '__main__':
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
